6b94256aba24cb21027bac2451de0b7606ba3b03
Ghost <ghostblade@pku.edu.cn>
2015-09-24 20:56:39 +0800

    add and commit

---
+++ FinchEngine/compositer.h
@@ -0,0 +1,26 @@
+#ifndef COMPOSITER_H
+#define COMPOSITER_H
+
+#include <QGL>
+#include <QOpenGLFunctions_4_3_Core>
+#include "material.h"
+
+class Compositer
+{
+public:
+	Compositer(QOpenGLFunctions_4_3_Core* func, int swidth, int sheight);
+	~Compositer();
+
+private:
+	QOpenGLFunctions_4_3_Core* m_func;
+	
+	GLuint m_vao;
+	GLuint m_fbo, m_dbo;
+	TexturePtr m_screen;
+	
+	GLuint m_fboMSAA, m_colorMSAA, m_depthMSAA;
+};
+
+#endif
+
+
